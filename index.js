681d8f8680f57e69d79118a65b3bcf414671c8e5
Nagateja Valiveti <teja91.valiveti@gmail.com>
2024-05-10 00:35:15 +0530

    registration added

---
+++ index.js
@@ -0,0 +1,111 @@
+// Menu Bar
+
+const menuBarButton = document.querySelector('.app__navbar > button');
+const menu = document.querySelector('.nav__menu');
+
+const toggleMenuBar = () => {
+    menu.classList.toggle('active__menu');
+    menuBarButton.classList.toggle('active__menu');
+}
+
+// Menu Bar
+
+// Services        
+
+const services = [
+    {
+        id: 1,
+        title: 'Hospitality Services',
+        description: 'Tailored staffing solutions for the corporate world, ensuring seamless operations and productivity.',
+        thumbnail: '/assets/S1.svg'
+    },
+    {
+        id: 2,
+        title: 'Part Time Job',
+        description: 'Elevating guest experiences with our trained and dedicated hospitality professionals, delivering excellence in service.',
+        thumbnail: '/assets/S2.svg'
+    },
+    {
+        id: 3,
+        title: 'Corporate Manpower Supply',
+        description: 'Flexible opportunities for event staffing, ensuring your gatherings are unforgettable with our skilled event personnel.',
+        thumbnail: '/assets/S3.svg'
+    },
+    {
+        id: 4,
+        title: 'Valet Driver',
+        description: 'Flexible opportunities for event staffing, ensuring your gatherings are unforgettable with our skilled event personnel.',
+        thumbnail: '/assets/S4.svg'
+    },
+    {
+        id: 5,
+        title: 'Bouncers',
+        description: 'Flexible opportunities for event staffing, ensuring your gatherings are unforgettable with our skilled event personnel.',
+        thumbnail: '/assets/S5.svg'
+    },
+    {
+        id: 6,
+        title: 'House Keeping',
+        description: 'Flexible opportunities for event staffing, ensuring your gatherings are unforgettable with our skilled event personnel.',
+        thumbnail: '/assets/S6.svg'
+    },
+    {
+        id: 7,
+        title: 'Kitchen Stewards',
+        description: 'Flexible opportunities for event staffing, ensuring your gatherings are unforgettable with our skilled event personnel.',
+        thumbnail: '/assets/S7.svg'
+    },
+    {
+        id: 8,
+        title: 'Chefs',
+        description: 'Flexible opportunities for event staffing, ensuring your gatherings are unforgettable with our skilled event personnel.',
+        thumbnail: '/assets/S8.svg'
+    },
+]
+
+const mainServices = document.querySelector('.services__main');
+
+services.forEach((service) => {
+    const serviceBox = document.createElement('div');
+    serviceBox.classList.add('service__box');
+
+    let serviceImage = document.createElement('img');
+    serviceImage.src = service.thumbnail;
+    serviceImage.alt = service.title;
+
+    let serviceDetail = document.createElement('div');
+    serviceDetail.classList.add('service__detail');
+    let serviceTitle = document.createElement('h4');
+    serviceTitle.textContent = service.title;
+    let serviceDesc = document.createElement('p');
+    serviceDesc.textContent = service.description;
+
+    serviceDetail.append(serviceTitle, serviceDesc);
+    serviceBox.append(serviceImage, serviceDetail);
+
+    mainServices.append(serviceBox);
+})
+
+// Services
+
+// Lassi Story
+
+const pics = Array.from(document.querySelectorAll('.lassi__pics'));
+
+let currentPicIndex = 0;
+
+setInterval(() => {
+    pics.forEach((pic, index) => {
+        if (currentPicIndex === index) {
+            pic.classList.add('active__img');
+        } else {
+            pic.classList.remove('active__img');
+        }
+
+    })
+    
+    currentPicIndex = currentPicIndex === pics.length - 1 ? 0 : currentPicIndex + 1;
+
+}, 2000);
+
+// Lassi Story
---
+++ registration.js
@@ -0,0 +1,197 @@
+let currentRegisterSection = 0;
+
+function updateToNextSection() {
+    const allRegisteredSection = Array.from(document.querySelectorAll('.register__section'));
+    allRegisteredSection.forEach((section) => {
+        section.classList.add('hide__section')
+        allRegisteredSection[currentRegisterSection + 1].classList.remove('hide__section');
+    })
+    currentRegisterSection += 1;
+}
+
+// Toastify
+
+function createToast(message) {
+    return Toastify({
+        text: message,
+        duration: 6000,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "var(--c-mainBg)",
+          color: "var(--c-primary)",
+          borderRadius: '8px',
+          boxShadow: '0 0 4px 1px var(--mainShadow)',
+          fontSize: '0.9rem',
+          padding: '0.8rem',
+          border: '1px solid var(--c-primary)'
+        },
+    }).showToast();
+}
+
+// Toastify
+
+const allServices = [
+    {
+        id: "85ac065f-ade0-4d5c-8e0f-c8d2d0b47101",
+        service: 'Hospitality Services'
+    },
+    {
+        id: "adf6df2a-6fbd-4daa-b225-4f2d1e09a957",
+        service: 'Parttime Jobs'
+    },
+    {
+        id: "375bee3b-b3b8-42a5-87b5-423cbc01fe33",
+        service: 'corporate Manpower Supply'
+    },
+    {
+        id: "a7814fbf-322c-44ff-a01a-dcee0dad3dc3",
+        service: 'Valet Driver'
+    },
+    {
+        id: "0e7345c1-f8bc-45aa-9181-db2855b09ac8",
+        service: 'Bouncers'
+    },
+    {
+        id: "99b5fdd2-bf49-499c-839b-0a37c84f4a06",
+        service: 'House Keepers'
+    },
+    {
+        id: "0b514b81-84fc-42d8-b500-dd5e42df1ad3",
+        service: 'Kitchen Stewards'
+    },
+    {
+        id: "fcfe522c-3daa-46d1-adf1-e28fc19fe75d",
+        service: 'Chefs'
+    },
+];
+
+const serviceSelectBox = document.querySelector('#service');
+
+allServices.forEach((service) => {
+    let serviceOption = document.createElement('option');
+    serviceOption.value = service.id;
+    serviceOption.id = service.id;
+    serviceOption.textContent = service.service;
+
+    serviceSelectBox.append(serviceOption);
+})
+
+function selectedFile(e, mainId) {
+
+    const msgPara = document.querySelector(`#${mainId} #msg__show`);
+    
+    if (e.target.files) {
+        const fileName = e.target.files[0];
+        fileName && msgPara.classList.add('show__selected');
+    }
+}
+
+// Handle Personal Details Submit
+
+let submitDetailsLoading = false;
+
+function btnInfo(isLoading) {
+    const detailSubmitBtn = document.querySelector('#detailsSubmitBtn');
+
+    detailSubmitBtn.disabled = isLoading;
+    detailSubmitBtn.innerHTML = isLoading ? '<span class="btnLoader"></span>' : 'Submit Details';
+}
+
+async function newRegistration(registeredDetails) {
+    try {
+        
+        btnInfo(true);
+        createToast('Verifying Details...');
+        const newRegistraionResponse = await fetch(`https://manpower-backend-shk1.onrender.com/api-prof/v1/auth/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(registeredDetails)
+        });
+
+        if (!newRegistraionResponse.ok) {
+            if (newRegistraionResponse.status === 409) {
+                createToast('Email or username already exists');
+            }
+            btnInfo(false);
+            return '';
+        }
+
+        const registerData = await newRegistraionResponse.json();
+        return registerData;
+
+    } catch (error) {
+        console.log(error);
+        return '';
+    }
+}
+
+async function handlePersonalDetailsSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    
+    const fullName = formData.get('fullName');
+    const email = formData.get('email');
+    const age = parseInt(formData.get('age'));
+    const password = formData.get('password');
+    const username = formData.get('username');
+    const mobileNumber = parseInt(formData.get('mobileNumber'));
+
+    const phoneNumber = parseInt(formData.get('phoneNumber'));
+    const whatsappNumber = parseInt(formData.get('whatsappNumber'));
+    const alternateNumber = parseInt(formData.get('alternateNumber'));
+
+    const contactNumbers = { phoneNumber, whatsappNumber, alternateNumber };
+
+    const permanent = formData.get('permanent');
+    const current = formData.get('current');
+    const latitude = parseFloat(formData.get('latitude'));
+    const longitude = parseFloat(formData.get('longitude'));
+
+    const address = { permanent, current, latitude, longitude };
+
+    const education = formData.get('education');
+
+    const number = parseInt(formData.get('number'));
+    const IFSCCode = formData.get('IFSCCode');
+    const bankName = formData.get('bankName');
+    const holderName = formData.get('holderName');
+
+    const accountDetails = { number, bankName, IFSCCode, holderName };
+
+    const upiId = formData.get('upiId');
+    const workExperience = formData.get('workExperience');
+    const language = formData.get('language');
+    
+    const dutyHours = parseInt(formData.get('dutyHours'));
+    const payment = dutyHours === 4 ? 400 : 700;
+
+    const service = formData.get('service');
+    const services = [service];
+
+    
+    if (!fullName && !email && !password && !age && !mobileNumber && !username && !permanent && !current && !latitude && !longitude && !education && !phoneNumber && !whatsappNumber && !number && !holderName && !bankName && !IFSCCode && !upiId && !workExperience && !language && !dutyHours && !payment && !service) {
+        createToast('Fill the required fields!');
+        return '';
+    }
+
+    const registerDetails = { fullName, email, password, age, mobileNumber, username, address, education, contactNumbers, accountDetails, upiId, workExperience, language, dutyHours, payment, services };
+
+    console.log(registerDetails);
+    
+    const response = await newRegistration(registerDetails);
+    if (response.mesage === 'An email has been sent to your mail.') {
+        createToast(response.mesage);
+        btnInfo(false);
+        updateToNextSection();
+    }
+
+}
+
+function handleUploadDocumentsSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+}
